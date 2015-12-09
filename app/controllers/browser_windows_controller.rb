14651a4f510e7b36174aa6ec95a41af9a1e857e8
theInternauts <cricketw@wearetheinternauts.com>
2015-12-09 02:29:26 -0800

    adding views for devices/browser_windows

---
+++ app/controllers/browser_windows_controller.rb
@@ -0,0 +1,36 @@
+class BrowserWindowsController < ApplicationController
+	def index
+		@browser_windows = BrowserWindow.all
+	end
+
+	def show
+		@browser_window = BrowserWindow.find(params[:id])
+	end
+
+	def new
+		@browser_window = BrowserWindow.new
+		@browser_windows = BrowserWindow.all
+	end
+
+	def create
+		new_window = BrowserWindow.create(browser_window_params)
+		Device.find(params[:device_id]).browser_windows << new_window
+		redirect_to device_browser_window_path(new_window.device_id, new_window)
+	end
+
+	def update
+		browser_window = BrowserWindow.find(params[:id])
+		BrowserWindow.update!(browser_window_params)
+		redirect_to device_browser_window_path(browser_window.device_id, browser_window)
+	end
+
+	def edit
+		@browser_window = BrowserWindow.find(params[:id])	
+		@browser_windows = BrowserWindow.all	
+	end
+
+	private
+		def browser_window_params
+      params.require(:browser_window).permit(:name)
+    end
+end
