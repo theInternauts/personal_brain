2f707566e88395dd414a9767e8b18405f7350d5b
theInternauts <cricketw@wearetheinternauts.com>
2015-12-09 02:28:42 -0800

    nesting browser_window under devices

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'devices#index'
 
-  resources :devices
+  resources :devices do
+    resources :browser_windows
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
