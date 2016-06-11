9a602d114129976b6548f627d7637a8d7d5d9435
theInternauts <cricketw@wearetheinternauts.com>
2016-06-11 16:03:07 -0700

    correcting timestamps

--- db/migrate/20160514071104_create_tags_table.rb
+++ db/migrate/20160514071104_create_tags_table.rb
@@ -2,13 +2,14 @@ class CreateTagsTable < ActiveRecord::Migration
   def change
     create_table :tags do |t|
       t.string :name
+      t.timestamps null: :false
     end
 
     create_table :taggings do |t|
       t.references :tag, index: true
       t.references :bookmark, index: true
       t.string :type, null: :false
-      t.timestamp
+      t.timestamps null: :false
     end
   end
 end
