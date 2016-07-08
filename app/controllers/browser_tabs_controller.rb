c71aea3aeee7e71b26a9faf05002757be2ab1b4e
theInternauts <cricketw@wearetheinternauts.com>
2016-07-08 02:26:18 -0700

    add policy authorization to BrowserTab

--- app/controllers/browser_tabs_controller.rb
+++ app/controllers/browser_tabs_controller.rb
@@ -1,18 +1,22 @@
 class BrowserTabsController < ApplicationController
   def index
+  	authorize BrowserTab
 		@browser_tabs = BrowserTab.all
 	end
 
 	def show
 		@browser_tab = BrowserTab.find(params[:id])
+		authorize @browser_tab
 	end
 
 	def new
+		authorize BrowserTab
 		@browser_tab = BrowserTab.new
 		@browser_tabs = BrowserWindow.find(params[:browser_window_id]).browser_tabs
 	end
 
 	def create
+		authorize BrowserTab
 		new_tab = BrowserTab.create(browser_tab_params)
 		window = BrowserWindow.find(params[:browser_window_id])
 		window.browser_tabs << new_tab
@@ -21,13 +25,15 @@ class BrowserTabsController < ApplicationController
 
 	def update
 		browser_tab = BrowserTab.find(params[:id])
+		authorize browser_tab
 		BrowserTab.update(browser_tab, browser_tab_params)
 		redirect_to device_browser_window_path(browser_tab.device, browser_tab.browser_window)
 	end
 
 	def edit
-		@browser_tab = BrowserTab.find(params[:id])	
-		@browser_tabs = BrowserTab.all	
+		@browser_tab = BrowserTab.find(params[:id])
+		authorize @browser_tab
+		@browser_tabs = BrowserTab.all
 	end
 
 	private
