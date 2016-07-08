60b05dce5f6e6267e3ba39a94bfe5fa118bc0696
theInternauts <cricketw@wearetheinternauts.com>
2016-07-07 22:39:53 -0700

    these scopes should come in handy for throught the app

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,6 +4,8 @@ class Bookmark < ActiveRecord::Base
 	has_and_belongs_to_many :tags
 
 	default_scope { order(created_at: :desc) }
+	scope :owned_by, ->(user) { where(user: user) }
+	scope :public_bookmarks, -> { where(private: false) }
 
 	def owner
 		user
