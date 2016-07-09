f761d0ab7d2e5e778a76314c5ddcac9571622cdb
theInternauts <cricketw@wearetheinternauts.com>
2016-07-08 22:50:46 -0700

    adding basic policy for Tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,25 +1,30 @@
 class TagsController < ApplicationController
 	def index
+		authorize Tag
 		@page = params[:page] ? params[:page] : 1
 		@per_page = Kaminari.config.default_per_page
 		@tags = Tag.all.order(:name).page @page
 	end
 
 	def new
+		authorize Tag
 		@tag = Tag.new
 	end
 
 	def create
+		authorize Tag
 		Tag.create(tag_params)
 		redirect_to tags_path
 	end
 
 	def edit
 		@tag = Tag.find(params[:id])
+		authorize @tag
 	end
 
 	def update
 		tag = Tag.find(params[:id])
+		authorize tag
 		Tag.update(tag, tag_params)
 		redirect_to tags_path
 	end
@@ -31,9 +36,12 @@ class TagsController < ApplicationController
 
 	def show
 		@tag = Tag.find(params[:id])
+		authorize @tag
 	end
 
 	def search
+		# lock it down for now
+		authorize Tag
 		# should move this functionality to run directly from the web server ahead of Rails (metal)
 		# - needs to accept a second param, exclusion tags from the result (becasue they've been selected/applied already)
 		@tags = Tag.where("name LIKE ?", "%#{params[:q]}%")
