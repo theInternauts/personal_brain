4d89e7e036c90d55a807bd55c9643262abb9578c
theInternauts <cricketw@wearetheinternauts.com>
2016-07-12 00:16:24 -0700

    change :name to :full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   include Clearance::User
   extend FriendlyId
   friendly_id :username
-  validates_presence_of :username, :email, :name
+  validates_presence_of :username, :email, :full_name
   validates_uniqueness_of :username
 
   has_many :devices
