a572f3919d21c74cc3e273f60b1d2f759b576292
theInternauts <cricketw@wearetheinternauts.com>
2016-07-12 00:15:22 -0700

    updating bookmarks route to work when the id isnt' specified. Also adding custom routes for Clearance

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "users#new", as: "sign_up"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,6 +24,7 @@ Rails.application.routes.draw do
   end
 
   resources :bookmarks, except: [:show, :index]
+  get 'bookmarks' => 'bookmarks#index'
   get 'bookmarks/:user' => 'bookmarks#index'
   get 'bookmarks/:user/:id' => 'bookmarks#show'
   get 'bookmarks_search' => 'bookmarks#search', as: :bookmarks_search
