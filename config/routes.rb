d840d0fa4ad6fc3e5f8b2d380a0a89365b474d31
theInternauts <cricketw@wearetheinternauts.com>
2016-07-24 18:54:51 -0700

    adding 'search' to main nav and making it the home page

--- config/routes.rb
+++ config/routes.rb
@@ -83,6 +83,6 @@ Rails.application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  root 'devices#index'
+  root 'search#index'
   get ':user' => 'bookmarks#index', as: :user_bookmarks
 end
