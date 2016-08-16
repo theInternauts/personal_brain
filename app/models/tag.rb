c1efe7eac6d5240c938c701cc7ae251c664285a4
theInternauts <cricketw@wearetheinternauts.com>
2016-08-16 00:00:01 -0700

    ensure tag order

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,7 +6,7 @@ class Tag < ActiveRecord::Base
 	has_and_belongs_to_many :bookmarks
 	default_scope { order(name: :asc) }
 
-	scope :owned_by, ->(user) { joins(bookmarks: :user).where("bookmarks.user_id = ?", user.id).distinct }
+	scope :owned_by, ->(user) { joins(bookmarks: :user).where("bookmarks.user_id = ?", user.id).distinct.order(:name) }
 
 	def should_generate_new_friendly_id?
     name_changed? || super
