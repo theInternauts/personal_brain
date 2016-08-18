bd558be7b5d56635a2383e54e965a439e23a1985
theInternauts <cricketw@wearetheinternauts.com>
2016-08-18 00:15:15 -0700

    using a .try() in case there's no 'type' param

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
 	def search
 		# should move this functionality to run directly from the web server ahead of Rails (metal)
 		# - needs to accept a second param, exclusion tags from the result (becasue they've been selected/applied already)
-		@model_name = params[:type].downcase
+		@model_name = params[:type].try(:downcase)
 		if params[:q].blank?
 			@results = []
 		else
