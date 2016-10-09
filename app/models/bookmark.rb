93d366a6a27f1c981eb2aa88f00adb5927a176c9
theInternauts <cricketw@wearetheinternauts.com>
2016-10-09 15:40:43 -0700

    ensure there's a title set for new bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,7 @@ class Bookmark < ActiveRecord::Base
 	validates :url, presence: true, uniqueness: { message: "This bookmark already exists." }
 	belongs_to :user
 	has_and_belongs_to_many :tags
+	before_create :ensure_title
 
 	default_scope { order(created_at: :desc) }
 	scope :owned_by, ->(user) { where(user: user) }
@@ -9,5 +10,9 @@ class Bookmark < ActiveRecord::Base
 
 	def owner
 		user
+	end
+
+	def ensure_title
+		self.title = self.url if self.title.blank?
 	end
 end
